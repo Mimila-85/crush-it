3ee9cd4ec73da8218cba2b4fc26b7f39fe8ae94f
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-15 09:43:23 -0500

    Deleted require to unused Exercise model

--- controllers/api/routineRoutes.js
+++ controllers/api/routineRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Routine, Exercise } = require("../../models");
+const { Routine } = require("../../models");
 const withAuth = require("../../utils/auth")
 
 router.post("/", withAuth, async (req, res) => {
